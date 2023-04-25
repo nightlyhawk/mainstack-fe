34775f2c04f49e34ca5f1a685cda60a0ac8ee8f6
Senanyo Benwari <65190458+NightlyHawk@users.noreply.github.com>
2023-04-25 22:41:49 +0100

    Update dashBoard.tsx

--- src/components/dashBoard.tsx
+++ src/components/dashBoard.tsx
@@ -22,16 +22,16 @@ export const DashBoard = () => {
             </div>
             <div>
                 <h2><img src={db} alt="dashboard" />Dashboard</h2>
-                <NavLink> <img src={edit} alt="edit" /> item 1</NavLink>
-                <NavLink><img src={ran} alt="group" />item 2</NavLink>
-                <NavLink><img src={hourglass} alt="duration" />item 3</NavLink>
+                <NavLink to=""> <img src={edit} alt="edit" /> item 1</NavLink>
+                <NavLink to=""><img src={ran} alt="group" />item 2</NavLink>
+                <NavLink to=""><img src={hourglass} alt="duration" />item 3</NavLink>
                 <h3>PAYMENTS</h3>
-                <NavLink><img src={add_an_image} alt="add an image" />item 4</NavLink>
-                <NavLink><img src={trash} alt="delete" />item 5</NavLink>
+                <NavLink to=""><img src={add_an_image} alt="add an image" />item 4</NavLink>
+                <NavLink to=""><img src={trash} alt="delete" />item 5</NavLink>
                 <h3>BUSINESS TOOLS</h3>
-                <NavLink><img src={subscription} alt="subscription" />item 6</NavLink>
-                <NavLink><img src={file} alt="file present" />item 7</NavLink>
-                <NavLink><img src={alarm} alt="alarm" />item 8</NavLink>
+                <NavLink to=""><img src={subscription} alt="subscription" />item 6</NavLink>
+                <NavLink to=""><img src={file} alt="file present" />item 7</NavLink>
+                <NavLink to=""><img src={alarm} alt="alarm" />item 8</NavLink>
             </div>
 
          </div>
